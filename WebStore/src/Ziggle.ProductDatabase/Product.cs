0652901e5aeac8c2480ae118104098bdf593a6ec
jessicaoriondo <jessicao_36@yahoo.com>
2020-08-03 21:56:25 -0700

    Add categories to WebStore

---
+++ WebStore/src/Ziggle.ProductDatabase/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ziggle.ProductDatabase
+{
+    public partial class Product
+    {
+        public Product()
+        {
+            OrderItem = new HashSet<OrderItem>();
+            ShoppingCartItem = new HashSet<ShoppingCartItem>();
+        }
+
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int ProductQuantity { get; set; }
+
+        public ICollection<OrderItem> OrderItem { get; set; }
+        public ICollection<ShoppingCartItem> ShoppingCartItem { get; set; }
+    }
+}
---
+++ WebStore/src/Ziggle.WebSite/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Ziggle.WebSite.Models;
+using Ziggle.Business;
+
+namespace Ziggle.WebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var categories = categoryManager.Categories
+                                        .Select(t => new Ziggle.WebSite.Models.CategoryModel(t.Id, t.Name))
+                                        .ToArray();
+            var model = new IndexModel { Categories = categories };
+            return View(model);
+        }
+
+        private readonly ICategoryManager categoryManager;
+        public HomeController(ICategoryManager categoryManager)
+        {
+            this.categoryManager = categoryManager;
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
