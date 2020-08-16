b1aeb97245cc58298e51c1c277b93bf02208ac48
jessicaoriondo <jessicao_36@yahoo.com>
2020-08-16 16:51:05 -0700

    initialize HelloWorldService

---
+++ HelloWorldService/HelloWorldService/Controllers/ContactsController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloWorldService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloWorldService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        public static List<Contact> contacts = new List<Contact>();
+        public static int currentId = 101;
+
+        // GET: api/<ContactsController>
+        [HttpGet]
+        public IEnumerable<Contact> Get()
+        {
+            return contacts;
+        }
+
+        // GET api/<ContactsController>/5
+        [HttpGet("{id}")]
+        public Contact Get(int id)
+        {
+            var contact = contacts.FirstOrDefault(t => t.Id == id);
+            return contact;
+        }
+
+        // POST api/<ContactsController>
+        [HttpPost]
+        public void Post([FromBody] Contact value)
+        {
+            value.Id = currentId++;
+            contacts.Add(value);
+        }
+
+        // PUT api/<ContactsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Contact value)
+        {
+            var contact = contacts.FirstOrDefault(t => t.Id == id);
+            
+           // contact = value; //method is not safe because you dont want to touch/change other values
+
+            //safe way
+            contact.Id = id;
+            contact.Name = value.Name;
+            contact.Phones = value.Phones;
+        }
+
+        // DELETE api/<ContactsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            contacts.RemoveAll(t => t.Id == id);
+        }
+    }
+}
